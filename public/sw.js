4a0e2150abb09678aaa8b6152526274a0c6a3c40
Andrea Salinas Pal <Andre@LTMED-M122606.local>
2026-04-09 10:19:19 -0500

    forzar update cache

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,12 @@
-const CACHE_NAME = 'gastos-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/manifest.json',
-];
-
+const CACHE_NAME = 'gastos-v2';
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
-  );
   self.skipWaiting();
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
 });
-
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
